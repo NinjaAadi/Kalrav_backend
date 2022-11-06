1f76f46d5fcfb3fe9fb3e6ec073ea2756ae2889f
Aaditya Pal <aadityapal.info@gmail.com>
2022-11-06 14:37:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+
+/*
+    Name
+    Email
+    College Name
+    Events
+
+1.Solo Singing (Eastern &Western)
+2.Battle of Bands
+3.Rapping 
+4.Beatboxing 
+5.Instrumental 
+6.Solo Dance ( Eastern & Western)
+7.Group Dance Battle 
+8.Street Dance
+9.Street play 
+10.Fashion Show 
+11.Mr & Miss KAALRAV 
+12.Dj Wars
+13.Treasure Hunt
+14.Poster for sponsor
+15.Digital Art 
+16.Face Painting 
+17.Debate
+18.Tug of War
+19.E-Games
+20.Open mic
+21."A one day All India political parties meet (AIPPM)"
+
+Image
+
+*/
+app.listen(5000, () => {
+  console.log("App listening on port 5000!");
+});
